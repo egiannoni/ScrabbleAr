b04acac1ae967f873ad95b8b18fa549995f0612d
egiannoni <giannonieugenia@gmail.com>
2020-06-24 16:09:56 -0300

    modulo con funciones para abrir datos y cargar datos usando pickle (tdv no funciona)

---
+++ MODULO_BASEDEDATOS.py
@@ -0,0 +1,24 @@
+import pickle
+
+#############  APERTURA DE BASE DE DATOS #############
+def abro_base(nombre_archivo):
+    try:
+        with open(nombre_archivo , 'rb') as f:
+            y = pickle.load(f)
+    except 'EOFError':
+        with open(nombre_archivo , 'xb') as f:
+            y = pickle.load(f)
+
+    return y
+
+############# CARGA DE DATOS #############
+def agrego_a_base(nombre_archivo, datos):
+    with open(nombre_archivo, 'ab+') as f:
+        pickle.dump(datos, f)
+    f.close()
+
+
+#    except 'FileNotFoundError':
+#        print('se levanto la excepcion')
+#        with open(nombre_archivo, 'x') as f:
+#           y= pickle.load(f, encoding="ASCII", errors="strict", buffers=None)

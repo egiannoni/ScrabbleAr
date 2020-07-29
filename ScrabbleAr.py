dd941bcf217ed8fddb6efda742d13c24353d722a
egiannoni <62036247+egiannoni@users.noreply.github.com>
2020-07-29 12:46:46 -0300

    Add files via upload

--- ScrabbleAr.py
+++ ScrabbleAr.py
@@ -3,17 +3,18 @@ import ConoceMas
 import Ranking
 import Registro
 import Game
-
+from Jugadores import ListaJugadores
 
 layout = [  [sg.Text('Scrabble Ar', size=(15, 1), justification='center', font=("Times", "24", "bold italic"),text_color='black')],
-            [sg.T(' ' * 7),sg.InputText('Nick',size=(30, 1),text_color='gray',key='nick')],
-            [sg.T(' ' * 7),sg.InputText('Pass',size=(30, 1),text_color='gray',key='pass',password_char='*')],
+            [sg.Text(' Usuario: '),sg.InputText(size=(30, 1),text_color='gray',key='nick')],
+            [sg.T('Contraseña:'),sg.InputText(size=(30, 1),text_color='gray',key='password',password_char='*')],
             [sg.T(' ' * 20),sg.Button(' Iniciar Sesion  ')],
             [sg.T(' ' * 22),sg.Button('Registrarse')],
             [sg.T(' ' * 30)],
             [sg.Button('Conoce mas del juego'),sg.T(' ' * 5), sg.Button('Nuestro Ranking')] ]
 
 window1 = sg.Window('ScrabbleAr ',layout)
+
 
 while True:
     event,value=window1.read()
@@ -26,19 +27,18 @@ while True:
     if event == 'Registrarse':
         Registro.main()
     if event == ' Iniciar Sesion  ':
-        Game.main()
-        #nickingresado= value['nick']
-        #paswingresada= value['pasword']
-        #base=AbroBase('base_datos.pkl')
-        #try:
-        #    if nickingresado in base:
-        #        pasword= Jugador.get_pass()
-        #        try:
-        #            if paswingresada == pasword:
-        #                window1.close()
-        #                juego.main()
-        #        except:
-        #            sg.SystemTray.notify('Error', 'La contraseña ingresada no coincide ')
-        #
-        #except:
-        #    sg.SystemTray.notify('Error', 'el nick ingresado NO existe.')
+        usuario= value['nick']
+        password= value['password']
+        lista_jugadores=ListaJugadores()
+        try:
+            usuario_valido = False
+            for jugador in lista_jugadores.get_jugadores():
+                if jugador.get_nick() == usuario and jugador.get_password() == password:
+                    usuario_valido = True
+                    print ('validacion con exito')
+                    window1.close()
+                    Game.main()
+                else:
+                    sg.SystemTray.notify('Error', 'La contraseña ingresada no coincide ')
+        except:
+            sg.SystemTray.notify('Error', 'el nick ingresado NO existe.')
